966f173e6092e947265ebdc71b80cf03b4fb27a9
Kevin Alford <zeroeth@pixelflow.org>
2011-02-08 13:27:14 -0500

    - initial rails push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourLights::Application.config.secret_token = '66eb0ca994d2b888cf93b2d900326a1a44b5c3a4a94f953c5b5aec91906a408ce1754197a5fffe5cb8f553005922b195290e03fe4280943bbdf3690a33440cb0'
