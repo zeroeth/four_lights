c6f242d20f6c3d382baf87a56d32d94b8d4f31f5
Kevin Alford <zeroeth@pixelflow.org>
2011-02-08 16:38:30 -0500

    - added flash error for bad addresses

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -1,12 +1,18 @@
 class LegislatorsController < ApplicationController
   def all_for
-    if params[:latitude].present? && params[:longitude].present?
-      @legislators = Sunlight::Legislator.all_for :latitude => params[:latitude], :longitude => params[:longitude]
-    elsif params[:address].present?
-      @legislators = Sunlight::Legislator.all_for :address => params[:address]
-    else
-      render 'index'
-    end
+    begin
+      if params[:latitude].present? && params[:longitude].present?
+        @legislators = Sunlight::Legislator.all_for :latitude => params[:latitude], :longitude => params[:longitude]
+      elsif params[:address].present?
+        @legislators = Sunlight::Legislator.all_for :address => params[:address]
+      else
+        flash.now[:error] = "Please fill in your address."
+        render 'index'
+      end
+     rescue NoMethodError
+        flash.now[:error] = "Please enter a valid address."
+        render 'index'
+     end
   end
 
   def index
